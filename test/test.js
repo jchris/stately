7944f654ff4d19385608190782e8238ab679f5a2
Chris Anderson <jchris@apache.org>
2010-09-04 16:33:15 -0700

    added default state handlers

--- test/test.js
+++ test/test.js
@@ -93,6 +93,77 @@ vows.describe('Stately').addBatch({
         assert.isTrue(obj.default_ran);
       }
     }
+  },
+  "with a typed default" : {
+    topic : stately.define({
+      apple : {
+        ripe : function(apple) {
+          apple.bite = "chomp";
+        },
+        _default : function(apple) {
+          apple.apple_default_ran = true;
+        }
+      }
+    }),
+    "with a typed obj that doesn't match state" : {
+      topic : function(machine) {
+        var obj = {type:"apple",state : "nomatch"};
+        machine.handle(obj);
+        return obj;
+      },
+      "should run the default" : function(obj) {
+        assert.isTrue(obj.apple_default_ran);
+      }
+    },
+    "with an untyped object" : {
+      topic : function(machine) {
+        var obj = {state : "nomatch"};
+        machine.handle(obj);
+        return obj;
+      },
+      "should not run" : function(obj) {
+        assert.isUndefined(obj.apple_default_ran);
+      }
+    }
+  },
+  "with a generic default and typed default" : {
+    topic : stately.define({
+      apple : {
+        ripe : function(apple) {
+          apple.bite = "chomp";
+        },
+        _default : function(apple) {
+          apple.apple_default_ran = true;
+        }
+      },
+      _default : function(obj) {
+        obj.generic_default_ran = true;
+      }
+    }),
+    "with a typed obj that doesn't match state" : {
+      topic : function(machine) {
+        var obj = {type:"apple",state : "nomatch"};
+        machine.handle(obj);
+        return obj;
+      },
+      "should run the default" : function(obj) {
+        assert.isTrue(obj.apple_default_ran);
+      },
+      "should not run the generic default" : function(obj) {
+        assert.isUndefined(obj.generic_default_ran);
+      }
+    },
+    "with an untyped object" : {
+      topic : function(machine) {
+        var obj = {state : "nomatch"};
+        machine.handle(obj);
+        return obj;
+      },
+      "should run the generic default" : function(obj) {
+        assert.isUndefined(obj.apple_default_ran);
+        assert.isTrue(obj.generic_default_ran);
+      }
+    }
   }
 }).export(module);
 
