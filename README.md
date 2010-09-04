2eb9470f722629e3e4de98b2cc25e576c2b2eee7
Chris Anderson <jchris@apache.org>
2010-09-04 16:08:30 -0700

    add types

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 
 ## Usage
 
+## Tests
 
+Haters gonna hate, but I used [Vows](http://vowsjs.org) and "it works!"
+
+To run them:
+
+    npm install vows
+    vows test/test.js
+
+## License
+
+[Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
--- test/test.js
+++ test/test.js
@@ -16,7 +16,7 @@ vows.describe('Stately').addBatch({
       });
       return machine;
     },
-    "with a basic event" : {
+    "with a matching state" : {
       topic : function(machine) {
         var obj = {state : "foo"};
         machine.handle(obj);
@@ -25,6 +25,43 @@ vows.describe('Stately').addBatch({
       "should get triggered" : function(obj) {
         assert.equal(obj.foo, 'bar');
       }
+    },
+    "with a non-matching state" : {
+      topic : function(machine) {
+        var obj = {state : "bam"};
+        machine.handle(obj);
+        return obj;
+      },
+      "should not run" : function(obj) {
+        assert.equal(obj.state, 'bam');
+      }
+    }
+  },
+  "with types and states" : {
+    topic : function() {
+      return stately.define({
+        apple : {
+          ripe : function(apple) {
+            apple.bite = "chomp";
+          }
+        }
+      });
+    },
+    "with an obj that matches" : {
+      topic : function(machine) {
+        var obj = {
+          type : "apple",
+          state : "ripe"
+        };
+        machine.handle(obj);
+        return obj;
+      },
+      "should run" : function(obj) {
+        assert.equal(obj.bite, "chomp");
+      }
+    },
+    "with an obj that doesn't match" : {
+      
     }
   }
 }).export(module);
